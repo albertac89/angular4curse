0a65b793b8015aae64abba17ea0a5517d527c6cd
Albert Aige Cortasa <albert.aige@gft.com>
2017-04-20 10:01:08 +0200

    added services to pass data

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
+import {ShoppingListService} from "../shopping-list.service";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,14 +9,14 @@ import { Ingredient } from '../../shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @Output() addedShoppingListItem = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  add(name, amount) {
-    this.addedShoppingListItem.emit(new Ingredient(name.value, amount.value));
+  onaddItem(name, amount) {
+    const newIngredient = new Ingredient(name.value, amount.value);
+    this.slService.addIngredient(newIngredient);
   }
 }
