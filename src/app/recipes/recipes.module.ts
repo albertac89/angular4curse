e8e6cd9fa4284c19fd90653bf9c4ac3babb96bec
Albert Aige Cortasa <albert.aige@gft.com>
2017-04-28 13:06:31 +0200

    lazy loading

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,7 +7,7 @@ import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
 import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
-import {RecipesRoutingModule} from "./recipes-routing.modules";
+import {RecipesRoutingModule} from "./recipes-routing.module";
 import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
